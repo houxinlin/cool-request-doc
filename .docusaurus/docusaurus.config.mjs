72a071b7ab945468916e3dc75386e91ed5629219
houxinlin <houxinlin@seczone.cn>
2024-10-10 10:13:43 +0800

    更换主页图片

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -14,7 +14,7 @@ export default {
   "onBrokenLinks": "throw",
   "onBrokenMarkdownLinks": "warn",
   "customFields": {
-    "newTagline": "新一代IDEA接口调试插件(v2024.9.15)"
+    "newTagline": "新一代IDEA接口调试插件(v2024.10.15)"
   },
   "i18n": {
     "defaultLocale": "en",
